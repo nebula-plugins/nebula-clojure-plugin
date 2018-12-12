a9d9735e20969b7c4ff81f2d107951ea6ba1b309
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2018-12-12 13:55:25 -0800

    remove ClojureSourceDirectorySet

--- src/main/groovy/nebula/plugin/clojuresque/tasks/ClojureSourceSet.groovy
+++ src/main/groovy/nebula/plugin/clojuresque/tasks/ClojureSourceSet.groovy
@@ -1,10 +1,7 @@
 package nebula.plugin.clojuresque.tasks
 
-import org.gradle.api.Action
 import org.gradle.api.file.SourceDirectorySet
 
 interface ClojureSourceSet {
     SourceDirectorySet getClojure()
-
-    ClojureSourceSet clojure(Action<? super SourceDirectorySet> configureAction)
 }
--- src/main/groovy/nebula/plugin/clojuresque/tasks/DefaultClojureSourceSet.groovy
+++ src/main/groovy/nebula/plugin/clojuresque/tasks/DefaultClojureSourceSet.groovy
@@ -1,25 +1,18 @@
 package nebula.plugin.clojuresque.tasks
 
-import org.gradle.api.Action
 import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.model.ObjectFactory
 
 class DefaultClojureSourceSet implements ClojureSourceSet {
-    private final SourceDirectorySet clojure;
+    private final SourceDirectorySet clojure
 
     DefaultClojureSourceSet(String name, ObjectFactory objects) {
-        this.clojure = objects.sourceDirectorySet(name, name);
+        this.clojure = objects.sourceDirectorySet(name, name)
         this.clojure.getFilter().include("**/*.clj", "**/*.cljs", "**/*.cljc")
     }
 
     @Override
     SourceDirectorySet getClojure() {
         return clojure
-    }
-
-    @Override
-    ClojureSourceSet clojure(Action<? super SourceDirectorySet> configureAction) {
-        configureAction.execute(clojure)
-        return this
     }
 }
