f7ffac15094db684afabfd73de8fc09bf39313d7
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-08-08 11:15:00 -0700

    ignore ClojureExecActionSpec

--- src/main/groovy/nebula/plugin/clojuresque/tasks/ClojureExec.groovy
+++ src/main/groovy/nebula/plugin/clojuresque/tasks/ClojureExec.groovy
@@ -147,6 +147,7 @@ class ClojureExec extends ConventionTask implements JavaExecSpec {
     }
 
     public ClojureExec setMain(String mainClassName) {
+        throw new RuntimeException("this shouldn't be called")
         clojureExecAction.setMain(mainClassName);
         return this;
     }
