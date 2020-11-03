e1cdbdfe1ec1ea48e987d690f021a0b5761787b1
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-11-03 15:52:17 -0800

    ClojureTest: classpath is marked as InputFiles

--- src/main/groovy/nebula/plugin/clojuresque/tasks/ClojureTest.groovy
+++ src/main/groovy/nebula/plugin/clojuresque/tasks/ClojureTest.groovy
@@ -17,6 +17,7 @@ import kotka.gradle.utils.Delayed
 import nebula.plugin.clojuresque.Util
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.StopExecutionException
@@ -27,6 +28,7 @@ class ClojureTest extends ClojureSourceTask {
     @Internal
     def outputDir
 
+    @InputFiles
     @Classpath
     @Delayed
     def classpath
