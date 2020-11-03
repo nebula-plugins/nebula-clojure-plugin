c74ac6a1de8236c94a6636fd4f77bf80aeeac236
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-11-03 15:17:36 -0800

    ClojureTest: junitOutputDir is marked as OutputDirectory

--- src/main/groovy/nebula/plugin/clojuresque/tasks/ClojureTest.groovy
+++ src/main/groovy/nebula/plugin/clojuresque/tasks/ClojureTest.groovy
@@ -18,7 +18,7 @@ import nebula.plugin.clojuresque.Util
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.StopExecutionException
 import org.gradle.api.tasks.TaskAction
 
@@ -38,7 +38,7 @@ class ClojureTest extends ClojureSourceTask {
     @Input
     def junit = false
 
-    @OutputFile
+    @OutputDirectory
     @Delayed
     def junitOutputDir = null
 
